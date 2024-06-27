69aa599a2872685e93aa5bddfe263e6d940b65e6
Pratham Patil <pratham.patil0049@gmail.com>
2024-06-27 12:23:20 +0530

    Add project files.

---
+++ BankManagementSystem/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Classes;
+
+var account = new BankAccount("Steve", 1000);
+Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance");
+
+account.MakeWithDrawal(500, DateTime.Now, "Rent Payment");
+Console.WriteLine(account.Balance);
+
+account.MakeWithDrawal(250, DateTime.Now, "Friend Payment");
+Console.WriteLine(account.Balance);
+
+
+Console.WriteLine(account.GetAccountHistory());
+
+BankAccount invalidAccount;
+try
+{
+    invalidAccount = new BankAccount("invalid", -55);
+}
+catch (ArgumentOutOfRangeException e)
+{
+    Console.WriteLine("Exception caught creating account with negative balance");
+    Console.WriteLine(e.ToString);
+    return;
+}
+
+try { account.MakeWithDrawal(750, DateTime.Now, "Attempt to overdraw"); }
+catch (InvalidOperationException e)
+{
+    Console.WriteLine("Exception caught trying to overdraw");
+    Console.WriteLine(e.ToString);
+}
